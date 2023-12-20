00937dbc7f997132e5a6d1b18dd1badc4264500f
DANIEL SOTO <sotojose.d589@gmail.com>
2023-12-20 15:38:14 -0500

    save

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,23 +4,22 @@ import TriviaPage from "./TriviaPage";
 import WordGuesserComponent from "../components/WordGuesserComponent";
 
 const HomePage = () => {
-    const sectionStyle = {
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        backgroundColor: '#add8e6',
-        marginTop: "20px", // Adjust the marginTop value to bring the section closer to the top
-      };
+  const sectionStyle = {
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    backgroundColor: "#add8e6",
+    marginTop: "20px", // Adjust the marginTop value to bring the section closer to the top
+  };
 
   const horizontalContainersStyle = {
     display: "flex",
     justifyContent: "space-between",
-    marginTop: "40px", 
-    padding: "20px",    
-    height: "250px",  
-   
+    marginTop: "40px",
+    padding: "20px",
+    height: "250px",
   };
 
   const containerItemStyle = {
@@ -29,15 +28,15 @@ const HomePage = () => {
     border: "1px solid #999",
     padding: "20px",
     margin: "0 40px",
-    height: "100%",     // Set height to 100% to match the height of the horizontal containers
-    width: "250px",     // Adjust the width to make the containers wider
-    borderRadius: 15, 
+    height: "100%", // Set height to 100% to match the height of the horizontal containers
+    width: "250px", // Adjust the width to make the containers wider
+    borderRadius: 15,
   };
 
   const placeholderBoxStyle = {
     width: "300px",
     height: "200px",
-    backgroundColor: "#ddd", 
+    backgroundColor: "#ddd",
     border: "1px solid #999",
     padding: "20px",
     margin: "20px",
@@ -52,7 +51,10 @@ const HomePage = () => {
 
   return (
     <div>
-      <section style={{ ...sectionStyle, backgroundColor: '#add8e6' }} id="section1">
+      <section
+        style={{ ...sectionStyle, backgroundColor: "#add8e6" }}
+        id="section1"
+      >
         <div className="container">
           <h1>Welcome to Mystery Educator!</h1>
           <p>Home of Children's education & entertainment</p>
@@ -81,44 +83,62 @@ const HomePage = () => {
         </div>
       </section>
 
-      <section style={{ ...sectionStyle, backgroundColor: 'yellow' }} id="section2">
+      <section
+        style={{ ...sectionStyle, backgroundColor: "yellow" }}
+        id="section2"
+      >
         <div className="container">
-        <RPSComponent />
+          <RPSComponent />
           <div style={placeholderBoxStyle}>
             <p>Content for Section 2</p>
           </div>
         </div>
       </section>
 
-
-      <section style={{ ...sectionStyle, backgroundColor: 'green' }} id="section3">
+      <section
+        style={{ ...sectionStyle, backgroundColor: "green" }}
+        id="section3"
+      >
         <div className="container">
-          <TriviaPage/>
+          <TriviaPage />
           <div style={placeholderBoxStyle}>
             <p>Content for Section 3</p>
           </div>
         </div>
       </section>
 
-      <section style={{ ...sectionStyle, backgroundColor: 'red' }} id="section4">
+      <section
+        style={{ ...sectionStyle, backgroundColor: "red" }}
+        id="section4"
+      >
         <div className="container">
-          <WordGuesserComponent/>
+          <WordGuesserComponent />
           <div style={placeholderBoxStyle}>
             <p>Content for Section 4</p>
           </div>
         </div>
       </section>
-      
-      <section style={{ ...sectionStyle, backgroundColor: 'pink' }} id="section5">
-        <div className="container">
-          {/* Placeholder box */}
-          <div style={placeholderBoxStyle}>
-            <p>Content for Section 5</p>
+
+      <section
+        style={{ ...sectionStyle, backgroundColor: "pink" }}
+        id="section5">
+        <div style={horizontalContainersStyle}>
+          <div style={containerItemStyle}>
+            <button id="nasaBtn">NASA</button>
+          </div>
+          <div style={containerItemStyle}>
+            <button id="metBtn" onClick={fetch('http://localhost:8080/arts', {method: 'GET', headers: {'Content-Type': 'application/json'}})}>Metropolitan Museum of Art</button>
+          </div>
+          <div style={containerItemStyle}>
+            <button id="factsBtn">Facts</button>
           </div>
         </div>
       </section>
 
-      <section style={{ ...sectionStyle, backgroundColor: 'brown' }} id="section6">
+      <section
+        style={{ ...sectionStyle, backgroundColor: "brown" }}
+        id="section6"
+      >
         <div className="container">
           {/* Placeholder box */}
           <div style={placeholderBoxStyle}>
