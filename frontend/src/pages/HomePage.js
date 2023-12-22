1f7f5a753e7a426b92f419277422891f0b8de42f
Daniel King <danielking4684@gmail.com>
2023-12-22 03:43:44 -0500

    about&contact,rps,homeTop nearing completion

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,14 +2,17 @@ import React from "react";
 import RPSComponent from "../components/RPSComponent";
 import TriviaPage from "./TriviaPage";
 import WordGuesserComponent from "../components/WordGuesserComponent";
-import kingImage from '../images/king.png'
-import ortizImage from '../images/ortiz.png'
-import sotoImage from '../images/soto.png'
-import jacksonImage from '../images/jackson.png'
-import rogersImage from '../images/rogers.png'
-import devBackground from '../images/dev-background.jpg'
+import kingImage from '../images/king.png';
+import ortizImage from '../images/ortiz.png';
+import sotoImage from '../images/soto.png';
+import jacksonImage from '../images/jackson.png';
+import rogersImage from '../images/rogers.png';
+import devBackground from '../images/dev-background.jpg';
 import wallpaperImage from "../images/wallpaper.png";
 import rpsBackGround from "../images/rpsBackground.png";
+import controllerImage from  "../images/controller.jpg";
+import spaceImage from "../images/space.jpg";
+import teamImage from "../images/team.jpg";
 
 
 
@@ -110,6 +113,29 @@ const HomePage = () => {
     borderRadius: 15,
   };
 
+  const subContainerModel = {
+    width: "270px",
+    height: "220px",
+    padding: "3px",
+    borderRadius: 10,
+    textAlign: "center",
+    fontSize: "20px",
+    color: "white",
+    fontFamily: "Arial, sans-serif",
+  };
+
+  const buttonStyle = {
+    width: "100px",
+    height: "100px",
+    backgroundColor: "limeGreen",
+    cursor: "pointer",
+    borderRadius: "50%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+    fontWeight: "bold",
+  };
 
 /* Daniel King's container style const below this comment until sections begin */
 
@@ -152,72 +178,144 @@ const HomePage = () => {
   }
 
   return (
-    <div> 
-        <section style={{ ...sectionStyle, backgroundColor: '#add8e6' }} id="section1"> 
-        <div className="container">
-          <h1>Welcome to Mystery Educator!</h1>
-          <p>Home of Children's education & entertainment</p>
-
-          {/* Three evenly spaced containers */}
-          <div style={horizontalContainersStyle}>
+    <div >
+    <h1>Welcome to the Home Page!</h1>  {/* this is behind the nav bar... is it deprecated? */}
+    {/* Section 1 Begin */}
+        <section style={{ ...homeSectionStyle }} id="section1">
+      <div style={{ ...homeBannerStyle }}>
+        <div style={imageHorizontalContainers}>
+          <div style={containerModel}>
             <div
-              style={containerItemStyle}
+              style={{
+                ...imageContainerModel,
+                backgroundImage: `url(${controllerImage})`,
+              }}
+              onClick={() => scrollToSection("section2")}
+            ></div>
+            <div style={subContainerModel}>
+              <p>
+                Embark on a journey of fun and learning! Dive into educational
+                games like Rock, Paper, Scissors, explore trivia about art,
+                science, and history, and challenge your mind with word
+                guessers. Adventure awaits!
+              </p>
+            </div>
+            <div
+              style={buttonStyle}
               onClick={() => scrollToSection("section2")}
             >
-              <p>Container 1</p>
+              PLAY NOW
+            </div>
+          </div>
+
+          <div style={containerModel}>
+            <div
+              style={{
+                ...imageContainerModel,
+                backgroundImage: `url(${spaceImage})`,
+              }}
+            ></div>
+            <div style={subContainerModel}>
+              <p>
+                Begin an exciting journey of learning! Dive into the wonders
+                of science, art, and history. Ignite curiosity and exploration
+                in a world of endless possibilities. Join us for an
+                educational adventure!
+              </p>
             </div>
             <div
-              style={containerItemStyle}
+              style={buttonStyle}
               onClick={() => scrollToSection("section5")}
             >
-              <p>Container 2</p>
+              LEARN NOW
+            </div>
+          </div>
+
+          <div style={containerModel}>
+            <div
+              style={{
+                ...imageContainerModel,
+                backgroundImage: `url(${teamImage})`,
+              }}
+            ></div>
+            <div style={subContainerModel}>
+              <p>
+                Discover the magic behind the scenes! Dive into our "About Us"
+                page to meet the creators and explore the inspiration that
+                fuels this educational adventure. Join us on this fascinating
+                journey!
+              </p>
             </div>
             <div
-              style={containerItemStyle}
+              style={buttonStyle}
               onClick={() => scrollToSection("section6")}
             >
-              <p>Container 3</p>
+              MEET US
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section style={{ ...sectionStyle, backgroundColor: 'yellow' }} id="section2">
-        <div className="container">
+{/* Section 2 Begin */}
+    <section style={{...sectionStyle, backgroundImage: `url(${rpsBackGround})`,}}
+      id="section2" >
+      <div className="container">
+        <div style={{ ...placeholderBoxStyle, backgroundColor: 'rgba(255, 240, 200, 0.8)', borderRadius: '70px', }}>
         <RPSComponent />
-          <div style={placeholderBoxStyle}>
-            <p>Content for Section 2</p>
-          </div>
         </div>
-      </section>
+      </div>
+    </section>
 
+    <section
+      style={{ ...sectionStyle, backgroundColor: "green" }}
+      id="section3"
+    >
+      <div className="container">
+        <TriviaPage />
+        <div style={placeholderBoxStyle}>
+          <p>Content for Section 3</p>
+        </div>
+      </div>
+    </section>
 
-      <section style={{ ...sectionStyle, backgroundColor: 'green' }} id="section3">
-        <div className="container">
-          <TriviaPage/>
-          <div style={placeholderBoxStyle}>
-            <p>Content for Section 3</p>
-          </div>
+    <section
+      style={{ ...sectionStyle, backgroundColor: "red" }}
+      id="section4"
+    >
+      <div className="container">
+        <WordGuesserComponent />
+        <div style={placeholderBoxStyle}>
+          <p>Content for Section 4</p>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section style={{ ...sectionStyle, backgroundColor: 'red' }} id="section4">
-        <div className="container">
-          <WordGuesserComponent/>
-          <div style={placeholderBoxStyle}>
-            <p>Content for Section 4</p>
-          </div>
+    <section
+      style={{ ...sectionStyle, backgroundColor: "pink" }}
+      id="section5"
+    >
+      <div style={horizontalContainersStyle}>
+        <div style={containerItemStyle}>
+          <button id="nasaBtn">NASA</button>
         </div>
-      </section>
-      
-      <section style={{ ...sectionStyle, backgroundColor: 'pink' }} id="section5">
-        <div className="container">
-          {/* Placeholder box */}
-          <div style={placeholderBoxStyle}>
-            <p>Content for Section 5</p>
-          </div>
+        <div style={containerItemStyle}>
+          <button
+            id="metBtn"
+            onClick={fetch("http://localhost:8080/arts", {
+              method: "GET",
+              headers: { "Content-Type": "application/json" },
+            })}
+          >
+            Metropolitan Museum of Art
+          </button>
         </div>
-      </section>
+        <div style={containerItemStyle}>
+          <button id="factsBtn">Facts</button>
+        </div>
+      </div>
+    </section>
+
 
        {/* About and Contact section */}
        <section style={{ ...comboAboutAndContactSectionStyle, backgroundColor: 'brown' }} id="section6">
@@ -317,9 +415,9 @@ const HomePage = () => {
                 <body style={{...{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}}>
                   <body>123 Adventure Way, EcoVille, Earth </body>
                   <body>Phone: +1 (800) JAC-ADVS</body>
-                  <body>Email: info@jacenterprises.com</body>
+                  <body>Email: <a href="mailto:info@jacenterprises.com">info@jacenterprises.com</a></body>
                   <body>Social Media: @JacAdventures</body>
-                  <body>Website: www.jacenterprises.com</body>
+                  <body>Website: <a href="www.jacenterprises.com">www.jacenterprises.com</a></body>
                 
                 </body>
               </div>
